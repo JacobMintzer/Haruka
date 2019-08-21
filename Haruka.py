87637f0ee1b377d51ac18eab8e18e279c0fc194f
JacobMintzer <jrmintz3@gmail.com>
2019-08-21 00:30:01 -0400

    mod features

--- Haruka.py
+++ Haruka.py
@@ -80,63 +80,81 @@ def isTarget(msg):
 @bot.command(hidden=True)
 @commands.check(is_admin)
 async def purge(ctx,*,msgs:int=10):
-	if not inBotMod(ctx.message):
-		await ctx.author.send("use this in the right channel")
-		await ctx.message.delete()
-		return
-	await ctx.send("Are you sure you want to delete the last {0} messages on the server? Type 'YES' to confirm.".format(str(msgs)))
-	msg=await bot.wait_for('message', check=inBotMod)
-	if msg.content=="YES":
-		async with ctx.message.channel.typing():
-			await ctx.message.channel.purge(limit = msgs)
-			await ctx.send("purge complete")
+	rxnMsg=await ctx.send("Are you sure you want to delete the last {0} messages on the server? react {1} to confirm or {2} to cancel.".format(str(msgs),u"\U0001F5D1", "🚫" ))
+	await rxnMsg.add_reaction(u"\U0001F5D1")
+	await rxnMsg.add_reaction("🚫")
+	async with ctx.message.channel.typing():
+		try:
+			rxn, user=await bot.wait_for('reaction_add', check=adminRxn, timeout=60.0)
+			if str(rxn.emoji)==u"\U0001F5D1":
+				await ctx.message.channel.purge(limit = msgs)
+				await ctx.send("purge complete")
+			else:
+				await ctx.send("cancelling the purge")
+		except asyncio.TimeoutError:
+			await rxnMsg.delete()
 
+	return
+
+def adminRxn(rxn, user):
+	print(rxn.emoji)
+	if user.permissions_in(bot.get_channel(config["generalCh"])).administrator and not user.bot:
+		if str(rxn.emoji) in [u"\U0001F5D1","🔨","🚫"]:
+			return True
+	return False
 
 @bot.command(hidden=True)
 @commands.check(is_admin)
 async def ban(ctx,*,person: discord.Member):
-	if not inBotMod(ctx.message):
-		await ctx.author.send("use this in the right channel")
-		await ctx.message.delete()
-		return
 	global target
 	while target!=None:
 		time.sleep(10)
-	await ctx.send("Type 'PURGE' purge {0} and ban then, type 'BAN' to only ban them and type anything else to cancel".format(str(person)))
+	rxnMsg=await ctx.send("React {1} to purge {0} and ban then, react 🔨 to only ban them and react 🚫 to cancel".format(str(person),u"\U0001F5D1"))
 	async with ctx.message.channel.typing():
-		msg=await bot.wait_for('message', check=inBotMod)
-		if msg.content=="YES":
-			target = person
-			for ch in ctx.message.guild.text_channels:
-				await ch.purge(check=isTarget)
-			target=None
-			await ctx.send("purge complete")
-			await person.ban()
-		elif msg.content=="NO":
-			await person.ban()
-		else:
-			await ctx.send("cancelling the ban")
+		await rxnMsg.add_reaction(u"\U0001F5D1")
+		await rxnMsg.add_reaction("🔨")
+		await rxnMsg.add_reaction("🚫")
+		try:
+			rxn, user=await bot.wait_for('reaction_add', check=adminRxn, timeout=60.0)
+			if str(rxn.emoji)==u"\U0001F5D1":
+				target = person
+				for ch in ctx.message.guild.text_channels:
+					await ch.purge(check=isTarget)
+				target=None
+				await ctx.send("purge complete")
+				await person.ban()
+			elif str(rxn.emoji)=="🔨":
+				await person.ban()
+			else:
+				await ctx.send("cancelling the ban")
+		except asyncio.TimeoutError:
+			await rxnMsg.delete()
+		return
 
 
 @bot.command(hidden=True)
 @commands.check(is_admin)
 async def prune(ctx,*,person: discord.Member):
-	if not inBotMod(ctx.message):
-		await ctx.author.send("use this in the right channel")
-		await ctx.message.delete()
-		return
 	global target
 	while target!=None:
 		time.sleep(10)
-	await ctx.send("Are you sure you want to delete all messages in the past 2 weeks by {0}? Type 'YES' to confirm.".format(str(person)))
-	msg=await bot.wait_for('message', check=inBotMod)
-	if msg.content=="YES":
-		async with ctx.message.channel.typing():
-			target = person
-			for ch in ctx.message.guild.text_channels:
-				await ch.purge(check=isTarget)
-			target=None
-			await ctx.send("purge complete")
+	rxnMsg=await ctx.send("Are you sure you want to delete all messages in the past 2 weeks by {0}? React {1} to confirm or 🚫 to cancel.".format(str(person),u"\U0001F5D1"))
+	async with ctx.message.channel.typing():
+		await rxnMsg.add_reaction(u"\U0001F5D1")
+		await rxnMsg.add_reaction("🚫")
+		try:
+			rxn, user=await bot.wait_for('reaction_add', check=adminRxn, timeout=60.0)
+			if str(rxn.emoji)==u"\U0001F5D1":
+				target = person
+				for ch in ctx.message.guild.text_channels:
+					await ch.purge(check=isTarget)
+				target=None
+				await ctx.send("purge complete")
+			elif str(rxn.emoji)=="🚫":
+				await ctx.send("cancelling prune")
+		except asyncio.TimeoutError:
+			await rxnMsg.delete()
+		return
 
 #@bot.command(hidden=True)
 @commands.check(is_admin)
