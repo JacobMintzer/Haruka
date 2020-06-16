322f2d220baa9c5d4c528d0574507c5f0029dcd0
Jacob <jrmintz3@gmail.com>
2020-06-16 22:00:20 +0000

    moved sauce function to `Fun` and fixed how the LLAS lookup function handles limit breaks

--- Haruka.py
+++ Haruka.py
@@ -13,22 +13,27 @@ import io
 import json
 import datetime
 import pytz
-from cogs.utilities import MessageHandler,Utils,Checks
-from saucenao import SauceNao
+from cogs.utilities import MessageHandler, Utils, Checks
 import atexit
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.INFO)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+handler = logging.FileHandler(
+	filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter(
+	'%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-bot = commands.Bot(command_prefix=['$'], description="I may just be a bot, but I really do love my big sister Kanata! For questions about Haruka please visit 'https://discord​​.gg/qp7nuPC' or DM `Junior Mints#2525`",case_insensitive=True)
+bot = commands.Bot(command_prefix=[
+                   '$'], description="I may just be a bot, but I really do love my big sister Kanata! For questions about Haruka please visit 'https://discord​​.gg/qp7nuPC' or DM `Junior Mints#2525`", case_insensitive=True)
 
-cogList=['cogs.Music','cogs.Administration', 'cogs.Fun','cogs.GuildFunctions','cogs.events', 'cogs.setup']
+cogList = ['cogs.Music', 'cogs.Administration', 'cogs.Fun',
+           'cogs.GuildFunctions', 'cogs.events', 'cogs.setup']
 with open('Resources.json', 'r') as file_object:
-	bot.config=json.load(file_object)
-bot.messageHandler=MessageHandler.MessageHandler(bot.config,bot)
+	bot.config = json.load(file_object)
+bot.messageHandler = MessageHandler.MessageHandler(bot.config, bot)
+
+
 async def is_admin(ctx):
 	try:
 		if ctx.author.permissions_in(ctx.message.channel).administrator:
@@ -51,6 +56,7 @@ def is_admin_enabled():
 		return ctx.message.guild.id in ctx.bot.config["modEnabled"]
 	return commands.check(predicate)
 
+
 @bot.check
 def check_enabled(ctx):
 	if ctx.message.guild is None:
@@ -58,100 +64,106 @@ def check_enabled(ctx):
 	if ctx.message.guild.id in bot.config["enabled"]:
 		return True
 	return False
-	
+
+
 @bot.event
 async def on_ready():
 	for cog in cogList:
 		bot.load_extension(cog)
-	await bot.change_presence(activity = discord.Game("Making lunch for Kanata!", type=1))
+	await bot.change_presence(activity=discord.Game("Making lunch for Kanata!", type=1))
 	await bot.messageHandler.initRoles(bot)
 	guild = bot.get_guild(bot.config["nijiCord"])
 	bot.allRoles = guild.roles
 	print('Logged in as:\n{0} (ID: {0.id})'.format(bot.user))
-	guildList=""
-	totalUsers=0
+	guildList = ""
+	totalUsers = 0
 	for guild in bot.guilds:
-		guildList=guildList+guild.name+", "
-		totalUsers+=guild.member_count
-	print("Currently in the current guilds: {0} with a total userbase of {1}".format(guildList,totalUsers))
+		guildList = guildList + guild.name + ", "
+		totalUsers += guild.member_count
+	print("Currently in the current guilds: {0} with a total userbase of {1}".format(
+		guildList, totalUsers))
+
 
 @bot.event
 async def on_command_error(ctx, error):
-	if isinstance(error,KeyboardInterrupt):
+	if isinstance(error, KeyboardInterrupt):
 		print("is keyboard interrupt")
 		await shutdown()
-	ignored=(commands.CommandNotFound,commands.UserInputError)
+	ignored = (commands.CommandNotFound, commands.UserInputError)
 	if hasattr(ctx.command, 'on_error'):
 		print(error)
 		return
-	if isinstance(error,ignored):
+	if isinstance(error, ignored):
 		return
-	print ("error in {0}".format(str(ctx.guild)))
+	print("error in {0}".format(str(ctx.guild)))
 	try:
-		logger.error("Error in {0} with message {1}".format(str(ctx.guild),str(ctx.message.clean_content)))
+		logger.error("Error in {0} with message {1}".format(
+			str(ctx.guild), str(ctx.message.clean_content)))
 	except:
-		logger.error("Message that caused error either doesn't exist or can't be found")
+		logger.error(
+			"Message that caused error either doesn't exist or can't be found")
 	raise (error)
+
 
 async def shutdown():
 	print("shutting down messagehandler")
 	bot.messageHandler.disconnect()
 	print("shutting down music cog")
-	music=bot.get_cog("Music")
+	music = bot.get_cog("Music")
 	await music.kill()
-	print ("killed music cog")
+	print("killed music cog")
 	for cog in cogList:
 		print("shutting down {0}".format(cog))
 		bot.unload_extension(cog)
 		print("shut down {0}".format(cog))
 	print("shutdown complete")
-	x=bot.logout()
+	x = bot.logout()
 	print("logging out")
 	await x
 
+
 @Checks.is_me()
 @bot.command(hidden=True)
-async def softReset(ctx,*,selectedCogs=None):
-	await bot.change_presence(activity = discord.Game("Quickly doing a soft-reset for a minor update, will be back up soon!", type=1))
+async def softReset(ctx, *, selectedCogs=None):
+	await bot.change_presence(activity=discord.Game("Quickly doing a soft-reset for a minor update, will be back up soon!", type=1))
 	if selectedCogs is None:
-		msg=True
-		cogs=cogList
+		msg = True
+		cogs = cogList
+		selectedCogs = ""
 	elif "messageHandler" in selectedCogs:
 		cogs = selectedCogs.split(" ")
 		cogs.remove("messageHandler")
-		msg=True
+		msg = True
 	else:
-		cogs=selectedCogs.split(" ")
-		msg=False
+		cogs = selectedCogs.split(" ")
+		msg = False
 	if msg:
 		bot.messageHandler.disconnect()
-	music=bot.get_cog("cogs.Music")
+	music = bot.get_cog("cogs.Music")
 	if "cogs.Music" in selectedCogs or "Music" in selectedCogs:
 		try:
 			await music.kill()
 		except:
 			print("no music service active")
-	print ("about to unload cogs")
+	print("about to unload cogs")
 	for cog in cogs:
 		try:
-			bot.unload_extension("cogs."+cog.replace("cogs.",""))
+			bot.unload_extension("cogs." + cog.replace("cogs.", ""))
 		except Exception as e:
-			print ("cannot unload cog {0}".format(cog))
+			print("cannot unload cog {0}".format(cog))
 	print("cogs unloaded, reloading everything now")
 
-	
 	with open('Resources.json', 'r') as file_object:
-		bot.config=json.load(file_object)
+		bot.config = json.load(file_object)
 	if msg:
 		importlib.reload(MessageHandler)
-		bot.messageHandler=MessageHandler.MessageHandler(bot.config,bot)
-
+		bot.messageHandler = MessageHandler.MessageHandler(bot.config, bot)
 
 	for cog in cogs:
 		try:
-			bot.load_extension("cogs."+cog.replace("cogs.",""))
+			bot.load_extension("cogs." + cog.replace("cogs.", ""))
 		except Exception as e:
-			await ctx.send("WARNING: cog {0} was not successfully reloaded for reason:\n`{1}`".format(cog,str(e)))
+			await ctx.send("WARNING: cog {0} was not successfully reloaded for reason:\n`{1}`".format(cog, str(e)))
 	if msg:
 		try:
 			await bot.messageHandler.initRoles(bot)
@@ -160,13 +172,16 @@ async def softReset(ctx,*,selectedCogs=None):
 	guild = bot.get_guild(bot.config["nijiCord"])
 	bot.allRoles = guild.roles
 	print('Logged in as:\n{0} (ID: {0.id})'.format(bot.user))
-	guildList=""
-	totalUsers=0
+	guildList = ""
+	totalUsers = 0
 	for guild in bot.guilds:
-		guildList=guildList+guild.name+", "
-		totalUsers+=guild.member_count
-	print("Currently in the current guilds: {0} with a total userbase of {1}".format(guildList,totalUsers))
-	await bot.change_presence(activity = discord.Game("Making lunch for Kanata!", type=1))
+		guildList = guildList + guild.name + ", "
+		totalUsers += guild.member_count
+	print("Currently in the current guilds: {0} with a total userbase of {1}".format(
+		guildList, totalUsers))
+	await bot.change_presence(activity=discord.Game("Making lunch for Kanata!", type=1))
+	rxn = Utils.getRandEmoji(bot.emojis, "hug")
+	await ctx.message.add_reaction(rxn)
 
 
 @Checks.is_me()
@@ -175,30 +190,30 @@ async def kill(ctx):
 	print("shutting down messagehandler")
 	bot.messageHandler.disconnect()
 	print("shutting down music cog")
-	music=bot.get_cog("Music")
+	music = bot.get_cog("Music")
 	await music.kill()
-	print ("killed music cog")
+	print("killed music cog")
 	for cog in cogList:
 		print("shutting down {0}".format(cog))
 		bot.unload_extension(cog)
 		print("shut down {0}".format(cog))
 	print("shutdown complete")
-	x=bot.logout()
+	x = bot.logout()
 	print("logging out")
 	await x
 
 
 @bot.event
 async def on_message(message):
-	await bot.messageHandler.handleMessage(message,bot)
+	await bot.messageHandler.handleMessage(message, bot)
 	return
 
 
 @bot.command(hidden=True)
 @commands.check(is_admin)
-async def s(ctx,*,msg=""):
-	fileList=[discord.File(io.BytesIO(await x.read(use_cached=True)),filename=x.filename) for x in ctx.message.attachments]
-	await ctx.send(msg,files=fileList)
+async def s(ctx, *, msg=""):
+	fileList = [discord.File(io.BytesIO(await x.read(use_cached=True)), filename=x.filename) for x in ctx.message.attachments]
+	await ctx.send(msg, files=fileList)
 	await ctx.message.delete()
 
 
@@ -207,58 +222,6 @@ async def git(ctx):
 	"""Link to Haruka's source code, and information related to the development"""
 	await ctx.send("Haruka was developed by Junior Mints#2525 and you can deliver any questions or comments to him. You can find the source code at https://github.com/JacobMintzer/Haruka \nIf you have any questions about it, feel free to message Junior Mints, or submit a pull request if you have any improvements you can make.")
 
-@bot.command(hidden=True)
-async def source(ctx, url: str=""):
-	"""Uses SauceNao to attempt to find the source of an image. Either a direct link to the image, or uploading the image through discord works"""
-	await sauce(ctx,url)
 
-@bot.command()
-async def sauce(ctx, url: str=""):
-	"""Uses SauceNao to attempt to find the source of an image. Either a direct link to the image, or uploading the image through discord works"""
-	async with ctx.typing():
-		try:
-			if (len(ctx.message.attachments)>0):
-				url=ctx.message.attachments[0].url
-			file="image."+url.split('.')[-1]
-			if (os.path.exists(file)):
-				os.remove(file)
-			request=requests.get(url,allow_redirects=True)
-			print (file)
-			open(file,'wb').write(request.content)
-		except:
-			await ctx.send("error downloading file")
-			return
-		foundSauce=""
-		output=SauceNao(directory='./', api_key=bot.config["sauce"])
-		result=output.check_file(file_name=file)
-		if (len(result)<1):
-			await ctx.send("no source found")
-			return
-		for source in result:
-			if (float(source["header"]["similarity"])<90):
-				break
-			if "Pixiv ID" in source["data"]["content"][0]:
-				await ctx.send("I believe the source is:\nhttps://www.pixiv.net/en/artworks/"+source["data"]["content"][0].split("Pixiv ID: ")[1].split("\n")[0])
-				return
-			elif "Source: Pixiv" in source["data"]["content"][0]:
-				stuff=source["data"]["content"][0].split("\n")[0]
-				id=stuff.split("Source: Pixiv #")[1]
-				await ctx.send("I believe the source is:\nhttps://www.pixiv.net/en/artworks/"+id)
-				return
-			elif "dA ID" in source["data"]["content"][0]:
-				foundSauce=("I believe the source is:\nhttps://deviantart.com/view/"+source["data"]["content"][0].split("dA ID: ")[1].split("\n")[0])
-			elif "Seiga ID:" in source["data"]["content"][0]:
-				foundSauce=("I believe the source is:\nhttps://seiga.nicovideo.jp/seiga/im"+source["data"]["content"][0].split("Seiga ID: ")[1].split("\n")[0])
-		if (foundSauce!=""):
-			await ctx.send(foundSauce)
-			return
-		if len(result[0]["data"]["ext_urls"])>0:
-			await ctx.send("I couldn't find the exact link, but this might help you find it:\n"+"\n".join(result[0]["data"]["ext_urls"]))
-			return
-		await ctx.send("sorry, I'm not sure what the source for this is.")
-
-
-
-with open("token.txt","r") as file_object:
+with open("token.txt", "r") as file_object:
 	bot.run(file_object.read().strip())
-
--- cogs/Fun.py
+++ cogs/Fun.py
@@ -3,32 +3,35 @@ import discord
 import requests
 import pprint
 import pandas as pd
+import os
+from saucenao import SauceNao
 from discord.ext import commands
 from .utilities import MessageHandler, Utils, Checks
 
+
 class Fun(commands.Cog):
-	def __init__(self,bot):
-		self.bot=bot
-		self.cooldown=[]
-	
+	def __init__(self, bot):
+		self.bot = bot
+		self.cooldown = []
+
 	@commands.group()
-	async def re(self,ctx,emote="",msg=""):
+	async def re(self, ctx, emote="", msg=""):
 		"""Searches for a random emote by search term. ex. '$re yay' will return a random 'yay' emote."""
 		if ctx.message.author.permissions_in(ctx.message.channel).administrator or ctx.message.guild is None:
 			if emote.lower() == "disable":
-				await self.disable(ctx,msg)
+				await self.disable(ctx, msg)
 				return
 			elif emote.lower() == "enable":
-				await self.enable(ctx,msg)
+				await self.enable(ctx, msg)
 				return
 			elif emote.lower() == "slowmode":
-				await self.slowmode(ctx,msg)
+				await self.slowmode(ctx, msg)
 				return
-		userHash=(str(ctx.message.guild.id)+str(ctx.message.author.id))
+		userHash = (str(ctx.message.guild.id) + str(ctx.message.author.id))
 		if ctx.message.channel.id in self.bot.config["reDisabled"]:
 			await ctx.message.add_reaction("❌")
 			return
-		emoji=Utils.getRandEmoji(ctx.bot.emojis, emote)
+		emoji = Utils.getRandEmoji(ctx.bot.emojis, emote)
 		if ctx.message.guild.id in self.bot.config["reSlow"]:
 			if userHash in self.cooldown:
 				await ctx.send("Please wait a little while before using this command again")
@@ -39,16 +42,16 @@ class Fun(commands.Cog):
 			await ctx.send(str(emoji))
 			if ctx.message.guild.id in self.bot.config["reSlow"]:
 				self.bot.loop.create_task(self.reCool(userHash))
-	
-	async def reCool(self,hash):
+
+	async def reCool(self, hash):
 		self.cooldown.append(hash)
 		await asyncio.sleep(15)
 		self.cooldown.remove(hash)
 
 	@re.command()
-	async def disable(self,ctx,msg=""):
+	async def disable(self, ctx, msg=""):
 		"""ADMIN ONLY!! Disables the $re command in this channel. Use `$re disable all` to disable everywhere. To reenable use `$re enable`"""
-		if msg.lower()=="all":
+		if msg.lower() == "all":
 			for channel in ctx.message.guild.text_channels:
 				if not(channel.id in self.bot.config["reDisabled"]):
 					self.bot.config["reDisabled"].append(channel.id)
@@ -58,9 +61,9 @@ class Fun(commands.Cog):
 		Utils.saveConfig(ctx)
 
 	@re.command()
-	async def enable(self,ctx,msg=""):
+	async def enable(self, ctx, msg=""):
 		"""ADMIN ONLY!! Enables $re in this channel. To enable everywhere type $re enable all"""
-		if msg.lower()=="all":
+		if msg.lower() == "all":
 			for channel in ctx.message.guild.text_channels:
 				if channel.id in self.bot.config["reDisabled"]:
 					self.bot.config["reDisabled"].remove(channel.id)
@@ -68,9 +71,9 @@ class Fun(commands.Cog):
 			if (ctx.message.channel.id in self.bot.config["reDisabled"]):
 				self.bot.config["reDisabled"].remove(ctx.message.channel.id)
 		Utils.saveConfig(ctx)
-		
+
 	@re.command()
-	async def slowmode(self,ctx,mode=""):
+	async def slowmode(self, ctx, mode=""):
 		"""ADMIN ONLY!! Adds cooldown per user of 15 seconds please say '$re slowmode' to toggle $re cooldown, or '$re slowmode on' or '$re slowmode off' to turn it on or off respectively."""
 		if mode is "":
 			if ctx.message.guild.id in self.bot.config["reSlow"]:
@@ -88,23 +91,23 @@ class Fun(commands.Cog):
 		Utils.saveConfig(ctx)
 
 	@commands.command()
-	async def e(self,ctx, emote=""):
+	async def e(self, ctx, emote=""):
 		"""Gets an emote from the server name. ex. $e aRinaPat."""
-		emoji=discord.utils.find(lambda emoji: emoji.name.lower() == emote.lower(),self.bot.emojis)
+		emoji = discord.utils.find(
+			lambda emoji: emoji.name.lower() == emote.lower(), self.bot.emojis)
 		if emoji is None:
 			await ctx.send("emoji not found")
 		else:
 			await ctx.send(str(emoji))
 
-
 	@commands.group()
 	@Checks.is_niji()
-	async def rank(self,ctx,*,idx="1"):
+	async def rank(self, ctx, *, idx="1"):
 		"""Gets message activity leaderboard. Optional page number. ex. '$rank 7' gets page 7 (ranks 61-70)"""
-		print (str(idx))
+		print(str(idx))
 		if str(idx).isdigit():
-			idx=int(idx)
-			await ctx.send(await self.bot.messageHandler.getPB(ctx.message.author,idx))
+			idx = int(idx)
+			await ctx.send(await self.bot.messageHandler.getPB(ctx.message.author, idx))
 		elif idx.lower() == 'ignore':
 			print("ignoring")
 			await self.ignore(ctx)
@@ -113,42 +116,42 @@ class Fun(commands.Cog):
 
 	@rank.command()
 	@Checks.hasBest()
-	async def best(self,ctx):
+	async def best(self, ctx):
 		"""Use this to show the rankings of what the most popular `best girl` role is"""
 		roleList = []
 		for roleName in ctx.bot.config["best"][str(ctx.message.guild.id)]:
-			role = discord.utils.find(lambda x: x.name.lower() == roleName.lower(), ctx.message.guild.roles)
-			roleList.append((role.name,len(list(filter(lambda x: role in x.roles , ctx.message.guild.members )))))
-		roleList.sort(key = lambda x: x[1],reverse=True) 
+			role = discord.utils.find(lambda x: x.name.lower(
+			) == roleName.lower(), ctx.message.guild.roles)
+			roleList.append((role.name, len(
+				list(filter(lambda x: role in x.roles, ctx.message.guild.members)))))
+		roleList.sort(key=lambda x: x[1], reverse=True)
 		series = pd.DataFrame(roleList)
-		series.index+=1
-		series.columns=["Best","Users"]
+		series.index += 1
+		series.columns = ["Best", "Users"]
 		await ctx.send("```fortran\n{0}```".format(series.to_string()))
-
-	
 
 	@commands.group()
 	@Checks.isScoreEnabled()
 	@Checks.is_admin()
-	async def score(self,ctx):
+	async def score(self, ctx):
 		"""Use `$score ignore` or `$score unignore` to add or remove a channel from the ignore list for Haruka's rankings"""
 		if ctx.invoked_subcommand is None:
 			await ctx.send("Use `$score ignore` or `$score unignore` to add or remove a channel from the ignore list for Haruka's rankings")
 
 	@score.command()
-	async def ignore(self,ctx,ch:discord.channel=None):
+	async def ignore(self, ctx, ch: discord.channel = None):
 		"""Use `$score ignore` to have Haruka ignore a channel"""
 		if ch is None:
 			ch = ctx.message.channel
 		try:
 			await ctx.send("ignoring {ch.mention}")
 		except:
-			print
+			pass
 		self.bot.config["scoreIgnore"].append(ch.id)
 		Utils.saveConfig(ctx)
 
 	@score.command()
-	async def unignore(self,ctx,ch:discord.channel=None):
+	async def unignore(self, ctx, ch: discord.channel = None):
 		if ch is None:
 			ch = ctx.message.channel
 		await ctx.send("No longer ignoring {ch.mention}")
@@ -156,67 +159,147 @@ class Fun(commands.Cog):
 			self.bot.config["scoreIgnore"].remove(ch.id)
 			Utils.saveConfig(ctx)
 		except:
-			print ("could not remove channel from scoreIgnore")
+			print("could not remove channel from scoreIgnore")
 
 	@commands.command()
-	async def llasID(self,ctx,id:int,lb=0):
+	async def llasID(self, ctx, id: int, lb="0"):
 		"""Search for a LLAS card by ID. Optionally give limit break level (defaults to 0). ex. '$llasID 146 2'"""
 		async with ctx.typing():
-			if len(str(lb))>1:
-				lb=int(lb[-1])
-			response=requests.get("http://all-stars-api.uc.r.appspot.com/cards/id/{0}".format(str(id)))
+			if lb.lower() == "mlb":
+				lb = 5
+			elif len(str(lb)) > 1:
+				lb = int(str(lb)[-1])
+			else:
+				lb = int(lb)
+			response = requests.get(
+				"http://all-stars-api.uc.r.appspot.com/cards/id/{0}".format(str(id)))
 			data = (response.json())
-			embd = self.embedCard(data,lb)
+			embd = self.embedCard(data, lb)
 			await ctx.send(embed=embd)
 
 	@commands.command()
-	async def llas(self,ctx,*,query):
+	async def llas(self, ctx, *, query):
 		"""Search for a LLAS card. ex. $llas Bowling Eli"""
 		async with ctx.typing():
-			lb=0
-			if query[-3:-1].lower()=="lb":
-				lb=int(query[-1])
-				query=query[:-3]
-			query=query.strip()
-			response=requests.get("http://all-stars-api.uc.r.appspot.com/cards/search?query={0}".format(str(query)))
-			data= (response.json())
-			if len(data)>15:
+			lb = 0
+			if query[-3:].lower() == "mlb":
+				lb = 5
+				query = query[:-3]
+			if query[-3:-1].lower() == "lb":
+				lb = int(query[-1])
+				query = query[:-3]
+			query = query.strip()
+			response = requests.get(
+				"http://all-stars-api.uc.r.appspot.com/cards/search?query={0}".format(str(query)))
+			data = (response.json())
+			if len(data) > 15:
 				await ctx.send("Too many potential cards, please send a more specific query.")
 				return
-			if len(data)>1:
+			if len(data) > 1:
 				await ctx.send(self.listCards(data))
 				return
-			if len(data)<1:
+			if len(data) < 1:
 				await ctx.send("No card found with given query.")
 				return
-			data=data[0]
-			embd=self.embedCard(data,lb)
+			data = data[0]
+			embd = self.embedCard(data, lb)
 			await ctx.send(embed=embd)
 
-	def embedCard(self,data,lb=0):
-		embd=discord.Embed(title= data["idolizedTitle"], description=data["title"],colour=discord.Colour(int(data["idol"]["color"].replace("#","0x"),16)))
-		embd.set_image(url="https://all-stars-api.uc.r.appspot.com/img/cards/{0}/iconI.jpg".format(data["id"]))
-		embd.set_thumbnail(url="https://all-stars-api.uc.r.appspot.com/img/cards/{0}/iconN.jpg".format(data["id"]))
-		rarity=discord.utils.find(lambda emoji: emoji.name.lower() == "LLAS{0}ICON".format(data["rarity"]["abbreviation"]).lower(),self.bot.emojis)
-		embd.set_author(name = data["idol"]["firstName"] + " " + data["idol"]["lastName"], icon_url = rarity.url)
-		if lb<0 or lb>5:
+	def embedCard(self, data, lb=0):
+		embd = discord.Embed(title=data["idolizedTitle"], description=data["title"], colour=discord.Colour(
+			int(data["idol"]["color"].replace("#", "0x"), 16)))
+		embd.set_image(
+			url="https://all-stars-api.uc.r.appspot.com/img/cards/{0}/iconI.jpg".format(data["id"]))
+		embd.set_thumbnail(
+			url="https://all-stars-api.uc.r.appspot.com/img/cards/{0}/iconN.jpg".format(data["id"]))
+		rarity = discord.utils.find(lambda emoji: emoji.name.lower() == "LLAS{0}ICON".format(
+			data["rarity"]["abbreviation"]).lower(), self.bot.emojis)
+		embd.set_author(name=data["idol"]["firstName"] +
+                  " " + data["idol"]["lastName"], icon_url=rarity.url)
+		if lb < 0 or lb > 5:
 			lb = 0
-		embd.add_field(name="Appeal (LB{0})".format(str(lb)), value=data["appeal"]["lb{0}".format(lb)])
-		embd.add_field(name="Stamina (LB{0})".format(str(lb)), value=data["stamina"]["lb{0}".format(lb)])
-		embd.add_field(name="Technique (LB{0})".format(str(lb)), value=data["technique"]["lb{0}".format(lb)])
-		embd.add_field(name="Skill", value=data["primaryActiveAbilityText"], inline=False)
-		embd.add_field(name="Passive Ability", value=data["passiveAbility"]["abilityText"], inline=False)
-		embd.add_field(name="Active Ability", value=data["secondaryActiveAbilityText"], inline=False)
-		embd.set_footer(text="{0}: ID: {1}".format(data["title"],data["id"]))
+		embd.add_field(name="Appeal (LB{0})".format(
+			str(lb)), value=data["appeal"]["lb{0}".format(lb)])
+		embd.add_field(name="Stamina (LB{0})".format(
+			str(lb)), value=data["stamina"]["lb{0}".format(lb)])
+		embd.add_field(name="Technique (LB{0})".format(
+			str(lb)), value=data["technique"]["lb{0}".format(lb)])
+		embd.add_field(
+			name="Skill", value=data["primaryActiveAbilityText"], inline=False)
+		embd.add_field(name="Passive Ability",
+                 value=data["passiveAbility"]["abilityText"], inline=False)
+		embd.add_field(name="Active Ability",
+                 value=data["secondaryActiveAbilityText"], inline=False)
+		embd.set_footer(text="{0}: ID: {1}".format(data["title"], data["id"]))
 		return embd
 
-
-
-	def listCards(self,data):
-		cardList="Multiple cards found, please pick the card you want with `$llasID <id>` with <id> replaced with the ID you want.```\nID  Rarity    Idol   Title      Idolized Title\n"
+	def listCards(self, data):
+		cardList = "Multiple cards found, please pick the card you want with `$llasID <id>` with <id> replaced with the ID you want.```\nID  Rarity    Idol   Title      Idolized Title\n"
 		for card in data:
-			cardList+="{0}.  {1} {2} {3}: {4} / {5}\n".format(card["id"],card["rarity"]["abbreviation"],card["idol"]["firstName"],card["idol"]["lastName"],card["title"],card["idolizedTitle"])
-		return cardList+"```"
-	
+			cardList += "{0}.  {1} {2} {3}: {4} / {5}\n".format(
+				card["id"], card["rarity"]["abbreviation"], card["idol"]["firstName"], card["idol"]["lastName"], card["title"], card["idolizedTitle"])
+		return cardList + "```"
+
+
+		
+	@commands.command(hidden=True)
+	async def source(self, ctx, url: str = ""):
+		"""Uses SauceNao to attempt to find the source of an image. Either a direct link to the image, or uploading the image through discord works"""
+		await self.sauce(ctx, url)
+
+
+	@commands.command()
+	async def sauce(self, ctx, url: str = ""):
+		"""Uses SauceNao to attempt to find the source of an image. Either a direct link to the image, or uploading the image through discord works"""
+		async with ctx.typing():
+			try:
+				if (len(ctx.message.attachments) > 0):
+					url = ctx.message.attachments[0].url
+				file = "image." + url.split('.')[-1]
+				if (os.path.exists(file)):
+					os.remove(file)
+				request = requests.get(url, allow_redirects=True)
+				print(file)
+				open(file, 'wb').write(request.content)
+			except Exception:
+				await ctx.send("error downloading file")
+				if (os.path.exists(file)):
+					os.remove(file)
+				return
+			foundSauce = ""
+			output = SauceNao(directory='./', api_key=self.bot.config["sauce"])
+			result = output.check_file(file_name=file)
+			if (os.path.exists(file)):
+				os.remove(file)
+			
+			if (len(result) < 1):
+				await ctx.send("no source found")
+				return
+			for source in result:
+				if (float(source["header"]["similarity"]) < 90):
+					break
+				if "Pixiv ID" in source["data"]["content"][0]:
+					await ctx.send("I believe the source is:\nhttps://www.pixiv.net/en/artworks/" + source["data"]["content"][0].split("Pixiv ID: ")[1].split("\n")[0])
+					return
+				elif "Source: Pixiv" in source["data"]["content"][0]:
+					stuff = source["data"]["content"][0].split("\n")[0]
+					id = stuff.split("Source: Pixiv #")[1]
+					await ctx.send("I believe the source is:\nhttps://www.pixiv.net/en/artworks/" + id)
+					return
+				elif "dA ID" in source["data"]["content"][0]:
+					foundSauce = ("I believe the source is:\nhttps://deviantart.com/view/" +
+					              source["data"]["content"][0].split("dA ID: ")[1].split("\n")[0])
+				elif "Seiga ID:" in source["data"]["content"][0]:
+					foundSauce = ("I believe the source is:\nhttps://seiga.nicovideo.jp/seiga/im" +
+					              source["data"]["content"][0].split("Seiga ID: ")[1].split("\n")[0])
+			if (foundSauce != ""):
+				await ctx.send(foundSauce)
+				return
+			if len(result[0]["data"]["ext_urls"]) > 0:
+				await ctx.send("I couldn't find the exact link, but this might help you find it:\n" + "\n".join(result[0]["data"]["ext_urls"]))
+				return
+			await ctx.send("sorry, I'm not sure what the source for this is.")
+
+
 def setup(bot):
 	bot.add_cog(Fun(bot))
