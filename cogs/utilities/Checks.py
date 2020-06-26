87f1d63e271454d3d8aedcdfdcc62cb3792f010a
JacobMintzer <jrmintz3@gmail.com>
2020-06-26 14:24:52 -0400

    Checks now give error messages that alert the user as to why they can't perform the function (except for only me functions, those should stay secret to most users).

--- cogs/utilities/Checks.py
+++ cogs/utilities/Checks.py
@@ -4,19 +4,25 @@ from discord.ext import commands
 
 
 def is_niji():
-	def predicate(ctx):
+	async def predicate(ctx):
 		if ctx.message.guild is None:
 			return False
-		return ctx.message.guild.id == ctx.bot.config["nijiCord"]
+		if ctx.message.guild.id == ctx.bot.config["nijiCord"]:
+			return True
+		else:
+			await ctx.message.channel.send("Sorry, this command is not available to all servers at the moment. If are interested in getting this to work here, please message Junior Mints#2525.")
+			return False
 	return commands.check(predicate)
 
 
 def is_admin():
-	def predicate(ctx):
+	async def predicate(ctx):
 		try:
 			if ctx.author.permissions_in(ctx.message.channel).administrator:
 				return True
-			return False
+			else:
+				await ctx.message.channel.send("This command is only available to an Administrator.")
+				return False
 		except Exception as e:
 			print(e)
 			return False
@@ -24,11 +30,15 @@ def is_admin():
 
 
 def isScoreEnabled():
-	def predicate(ctx):
+	async def predicate(ctx):
 		try:
 			if ctx.message.guild is None:
 				return False
-			return ctx.message.guild.id in ctx.bot.config["scoreEnabled"]
+			if ctx.message.guild.id in ctx.bot.config["scoreEnabled"]:
+				return True
+			else:
+				await ctx.message.channel.send("You must enable scoring on this server to use this command. An admin can enable it by using `$score enable`.")
+				return False
 		except Exception as e:
 			print(e)
 			return False
