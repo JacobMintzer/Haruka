e9073d0f93c595b307d6b7a800fad8f0e5893be0
JacobMintzer <jrmintz3@gmail.com>
2020-06-28 20:10:47 -0400

    I forgot adding error messages in command checks will fire them off when help was requested, so i removed them

--- cogs/utilities/Checks.py
+++ cogs/utilities/Checks.py
@@ -10,7 +10,6 @@ def is_niji():
 		if ctx.message.guild.id == ctx.bot.config["nijiCord"]:
 			return True
 		else:
-			await ctx.message.channel.send("Sorry, this command is not available to all servers at the moment. If are interested in getting this to work here, please message Junior Mints#2525.")
 			return False
 	return commands.check(predicate)
 
@@ -21,7 +20,6 @@ def is_admin():
 			if ctx.author.permissions_in(ctx.message.channel).administrator:
 				return True
 			else:
-				await ctx.message.channel.send("This command is only available to an Administrator.")
 				return False
 		except Exception as e:
 			print(e)
@@ -37,7 +35,6 @@ def isScoreEnabled():
 			if ctx.message.guild.id in ctx.bot.config["scoreEnabled"]:
 				return True
 			else:
-				await ctx.message.channel.send("You must enable scoring on this server to use this command. An admin can enable it by using `$score enable`.")
 				return False
 		except Exception as e:
 			print(e)
